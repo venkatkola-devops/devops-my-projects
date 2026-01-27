8b4ba4a44527a98d4a366c2798366194dff5a8ce
venkatkola-devops <venkatkola12@gmail.com>
2026-01-27 16:49:01 -0500

    feat: update app message

--- 01-ci-cd-webapp/app.py
+++ 01-ci-cd-webapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Client Web App - CI/CD Enabled"
+    return "Client Web App - Updated via PR"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
