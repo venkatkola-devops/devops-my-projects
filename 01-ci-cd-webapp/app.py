47e07170ff07d2d79424d6ca15879aa077d34c87
Whatisinaname <whatisinaname@macbookpro.home>
2026-01-27 20:02:55 -0500

    fix: local workflow and docker updates

--- 01-ci-cd-webapp/app.py
+++ 01-ci-cd-webapp/app.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def home():
     return "Client Web App - Updated via PR"
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
